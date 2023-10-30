61bc9ffed943a16ff4880392427743c76304e943
Alex <ak555110689@gmail.com>
2023-10-30 13:43:14 +0600

    refactoring messages view, debuf role choises

--- user_messages/views.py
+++ user_messages/views.py
@@ -6,7 +6,6 @@ from django.db.models import Q
 from profiles.models import Student, Teacher, Manager
 from .models import PrivateMessage
 from .forms import NewMessageForm
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -20,6 +19,7 @@ class Chat:
 
 def get_user_profile(user):
     role = None
+    profile = None
     if hasattr(user, 'student_profile'):
         profile = user.student_profile
         role = 'student'
@@ -31,71 +31,66 @@ def get_user_profile(user):
         role = 'manager'
     return profile, role
 
+def get_latest_message(profile, partner):
+    try:
+        return PrivateMessage.objects.filter(
+            Q(sender=partner, receiver=profile.user) | 
+            Q(sender=profile.user, receiver=partner)
+        ).latest('timestamp')
+    except PrivateMessage.DoesNotExist:
+        return None
+
+def add_chat(chats, partners, current_profile, user, current_role):
+    partner_profile, partner_role = get_user_profile(user)
+
+    if partner_profile.user not in partners:
+        partners.add(partner_profile.user)
+        try:
+            latest_message = PrivateMessage.objects.filter(
+                Q(sender=partner_profile.user, receiver=current_profile.user) |
+                Q(sender=current_profile.user, receiver=partner_profile.user)
+            ).latest('timestamp')
+            chats.append(Chat(partner_profile.user, latest_message, partner_role))
+        except Exception as e:
+            logger.error(f"Error getting latest message for user {user}: {e}")
+
 class MessageListView(ListView):
     model = PrivateMessage
     template_name = 'user_messages/message_list.html'
     context_object_name = 'chats'
 
     def get_queryset(self):
-        current_profile, role = get_user_profile(self.request.user)
-
+        profile, role = get_user_profile(self.request.user)
         partners = set()
         chats = []
-
-        messages_received = PrivateMessage.objects.filter(receiver=current_profile.user)
-        messages_sent = PrivateMessage.objects.filter(sender=current_profile.user)
-
-        logger.debug('Messages Received Query: %s', messages_received.query)
-        logger.debug('Messages Sent Query: %s', messages_sent.query)
+        messages_received = PrivateMessage.objects.filter(receiver=profile.user)
+        messages_sent = PrivateMessage.objects.filter(sender=profile.user)
 
         for msg in messages_received:
-            if msg.sender not in partners:
-                partners.add(msg.sender)
-                latest_message = PrivateMessage.objects.filter(
-                    Q(sender=msg.sender, receiver=current_profile.user) | 
-                    Q(sender=current_profile.user, receiver=msg.sender)
-                ).latest('timestamp')
-                chats.append(Chat(msg.sender, latest_message, role))
+            add_chat(chats, partners, profile, msg.sender, role)
 
         for msg in messages_sent:
-            if msg.receiver not in partners:
-                partners.add(msg.receiver)
-                latest_message = PrivateMessage.objects.filter(
-                    Q(sender=msg.receiver, receiver=current_profile.user) | 
-                    Q(sender=current_profile.user, receiver=msg.receiver)
-                ).latest('-timestamp')
-                chats.append(Chat(msg.receiver, latest_message, role))
-
-        logger.debug('Chats: %s', chats)
+            add_chat(chats, partners, profile, msg.receiver, role)
 
         return chats
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        if context['chats']:
+        profile, _ = get_user_profile(self.request.user)
+        
+        if 'chats' in context:
             for chat in context['chats']:
-                partner_id = chat.partner_id
-                partner_role = chat.partner_role
-
-                logger.debug(f"Partner ID: {partner_id}, Partner Role: {partner_role}")  # Debug output
-
-                partner = None
-                student = self.request.user.student_profile
-                context['student'] = student
-                
+                role_to_model = {
+                    'student': Student,
+                    'teacher': Teacher,
+                    'manager': Manager
+                }
                 try:
-                    if partner_role == 'student':
-                        partner = Student.objects.get(id=partner_id).user
-                    elif partner_role == 'teacher':
-                        partner = Teacher.objects.get(id=partner_id).user
-                    elif partner_role == 'manager':
-                        partner = Manager.objects.get(id=partner_id).user
-                except Exception as e:
-                    logger.error(f"Error retrieving partner from the database: {e}")
-
-                chat.partner_user = partner 
-
+                    partner_user = role_to_model[chat.partner_role].objects.get(id=chat.partner_id).user
+                    chat.partner_user = partner_user
+                except role_to_model[chat.partner_role].DoesNotExist:
+                    logger.error(f"Error retrieving partner with ID {chat.partner_id} and role {chat.partner_role}")
+        
         return context
 
 class MessageDetailView(ListView):
@@ -104,67 +99,49 @@ class MessageDetailView(ListView):
     context_object_name = 'messages'
 
     def get_queryset(self):
-        current_profile, role = get_user_profile(self.request.user)
-        receiver_role = self.kwargs['role']
-        partner_id = self.kwargs['receiver_id']
-        partner = get_object_or_404(User, pk=partner_id)
-
+        profile, _ = get_user_profile(self.request.user)
+        partner = get_object_or_404(User, pk=self.kwargs['receiver_id'])
         return PrivateMessage.objects.filter(
-            Q(sender=current_profile.user, receiver=partner) | 
-            Q(sender=partner, receiver=current_profile.user)
+            Q(sender=profile.user, receiver=partner) | 
+            Q(sender=partner, receiver=profile.user)
         ).order_by('-timestamp')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        current_profile, _ = get_user_profile(self.request.user)
-        receiver_role = self.kwargs['role']
-        partner_id = self.kwargs['receiver_id']
-        partner = get_object_or_404(User, pk=partner_id)
-
-        context['form'] = NewMessageForm()
-        context['receiver_role'] = receiver_role
-        context['receiver_id'] = partner_id
-        context['chat'] = {
-            'partner': partner,
-            'partner_role': receiver_role
-        }
-
+        partner = get_object_or_404(User, pk=self.kwargs['receiver_id'])
+        context.update({
+            'form': NewMessageForm(),
+            'receiver_role': self.kwargs['role'],
+            'receiver_id': self.kwargs['receiver_id'],
+            'chat': {
+                'partner': partner,
+                'partner_role': self.kwargs['role']
+            }
+        })
         return context
-    
+
     def post(self, request, *args, **kwargs):
         form = NewMessageForm(request.POST)
         if form.is_valid():
+            profile, _ = get_user_profile(self.request.user)
+            partner = get_object_or_404(User, pk=self.kwargs['receiver_id'])
             message = form.save(commit=False)
-            current_profile, _ = get_user_profile(self.request.user)
-            receiver_role = self.kwargs['role']
-            partner_id = self.kwargs['receiver_id']
-            partner = get_object_or_404(User, pk=partner_id)
-            
-            message.sender = current_profile.user
+            message.sender = profile.user
             message.receiver = partner
             message.save()
-            
-        return redirect('message_detail', role=receiver_role, receiver_id=partner_id)
-
+            return redirect('message_detail', role=self.kwargs['role'], receiver_id=self.kwargs['receiver_id'])
+        return HttpResponseNotAllowed(['POST'])
 
 def send_message(request):
     if request.method == 'POST':
         form = NewMessageForm(request.POST)
         if form.is_valid():
-            receiver_id = form.cleaned_data['receiver_id']
-            message_content = form.cleaned_data['content']
-
-            current_profile, role = get_user_profile(request.user)
-
+            profile, _ = get_user_profile(request.user)
             new_message = PrivateMessage(
-                content=message_content,
-                sender=current_profile.user,
-                receiver_id=receiver_id
+                content=form.cleaned_data['content'],
+                sender=profile.user,
+                receiver_id=form.cleaned_data['receiver_id']
             )
             new_message.save()
-
             return redirect(request.META.get('HTTP_REFERER', '/default/url/'))
-        else:
-            return HttpResponseNotAllowed(['POST'])
-    else:
-        return HttpResponseNotAllowed(['POST'])
+    return HttpResponseNotAllowed(['POST'])
